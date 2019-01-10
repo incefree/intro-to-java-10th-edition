0372f6a371aa12593b3426a475c3e6f4080530e7
Isaac Ince <45325737+incefree@users.noreply.github.com>
2019-01-09 19:36:25 -0600

    Update Exercise_08.java

--- Exercise_08.java
+++ Exercise_08.java
@@ -8,19 +8,20 @@ public class Ch6Page236Question9 {
 		for (double i = 1.0, j = 20.0; i <= 10; i++, j += 5) {
 			double meters = footToMeter(i);
 			double feet = meterToFoot(j);
-			
+
 			System.out.printf("%-10.1f %-10.3f    |    %9.1f  %12.3f\n", i, meters, j, feet);
-			
+
 		}
 		System.out.println();
-		
-		
+
 	}
-	//Convert from feet to meters 
+
+	// Convert from feet to meters
 	public static double footToMeter(double foot) {
 		return 0.305 * foot;
 	}
-	//Convert from meters to feet 
+
+	// Convert from meters to feet
 	public static double meterToFoot(double meter) {
 		return 3.279 * meter;
 	}
